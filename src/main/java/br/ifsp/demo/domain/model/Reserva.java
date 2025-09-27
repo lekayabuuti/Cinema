09d0fd32c377b078f582a3736b2e4a710a742c22
Letícia <leticia.yabuuti@aluno.ifsp.edu.br>
2025-09-27 18:51:09 -0300

    feat: criação da função de reservar ingressos

--- src/main/java/br/ifsp/demo/domain/model/Reserva.java
+++ src/main/java/br/ifsp/demo/domain/model/Reserva.java
@@ -1,19 +1,37 @@
 package br.ifsp.demo.domain.model;
 
-import br.ifsp.demo.controller.TransactionController;
+import jakarta.persistence.*;
+import java.util.UUID;
 
-import java.util.ArrayList;
-import java.util.List;
-
+@Entity
 public class Reserva {
-    private final String user;
-    private List<Ingresso> ingressos;
-    TransactionController transactionController;
 
-    public Reserva() {
-        this.user = "UID usuario";
-        this.ingressos = new ArrayList<>();
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(optional = false)
+    private SessaoCinema sessao;
+
+    @OneToOne(optional = false)
+    private Ingresso ingresso;
+
+    public Reserva() {}
+
+    public Reserva(SessaoCinema sessao, Ingresso ingresso) {
+        this.sessao = sessao;
+        this.ingresso = ingresso;
     }
 
+    public UUID getId() {
+        return id;
+    }
 
+    public SessaoCinema getSessao() {
+        return sessao;
+    }
+
+    public Ingresso getIngresso() {
+        return ingresso;
+    }
 }
