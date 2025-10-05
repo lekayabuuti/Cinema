fb61819fcbcd15268298aab7ea778ff1d01fb6a7
BrenoNLps <brenolopes.xy@gmail.com>
2025-10-05 03:13:28 -0300

    feat: criar entidade SessaoEntity com mapeamento JPA

--- src/main/java/br/ifsp/demo/entity/SessaoEntity.java
+++ src/main/java/br/ifsp/demo/entity/SessaoEntity.java
@@ -1,4 +1,69 @@
 package br.ifsp.demo.entity;
 
-public class SessaoEntity {
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "sessoes")
+public class SessaoEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate horarioData;
+    private LocalTime horarioHora;
+    private String filmeNome;
+    private int filmeMinutos;
+
+    @Column(name = "numero_sala")
+    private Integer numeroSala;
+
+    public SessaoEntity (){};
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getHorarioData() {
+        return horarioData;
+    }
+    public void setHorarioData(LocalDate horarioData) {
+        this.horarioData = horarioData;
+    }
+
+
+    public LocalTime getHorarioHora() {
+        return horarioHora;
+    }
+    public void setHorarioHora(LocalTime horarioHora) {
+        this.horarioHora = horarioHora;
+    }
+
+    public String getFilmeNome() {
+        return filmeNome;
+    }
+    public void setFilmeNome(String filmeNome) {
+        this.filmeNome = filmeNome;
+    }
+
+    public int getFilmeMinutos() {
+        return filmeMinutos;
+    }
+    public void setFilmeMinutos(int filmeMinutos) {
+        this.filmeMinutos = filmeMinutos;
+    }
+
+    public Integer getNumeroSala() {
+        return numeroSala;
+    }
+    public void setNumeroSala(Integer numeroSala) {
+        this.numeroSala = numeroSala;
+    }
 }
