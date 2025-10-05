3c0b3b833d77a514368640a33a0de17d6254aaf6
BrenoNLps <brenolopes.xy@gmail.com>
2025-10-05 04:43:56 -0300

    feat: validar data inicial e aplicar mapeamento com SessaoMapper

--- src/main/java/br/ifsp/demo/service/SessaoService.java
+++ src/main/java/br/ifsp/demo/service/SessaoService.java
@@ -1,7 +1,9 @@
 package br.ifsp.demo.service;
 
 import br.ifsp.demo.domain.model.Sessao;
+import br.ifsp.demo.exception.*;
 import br.ifsp.demo.repository.SessaoRepository;
+import br.ifsp.demo.mapper.SessaoMapper;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -10,12 +12,20 @@ import java.util.List;
 @Service
 public class SessaoService {
     private final SessaoRepository repository;
+    private final SessaoMapper mapper;
 
-    public SessaoService(SessaoRepository repository) {
+    public SessaoService(SessaoRepository repository, SessaoMapper mapper) {
         this.repository = repository;
+        this.mapper = mapper;
     }
 
     public List<Sessao> buscarSessoesEntre(LocalDate dataInicial, LocalDate dataFinal) {
-        return repository.buscarEntreDatas(dataInicial,dataFinal);
+        if(dataInicial.isAfter(LocalDate.now().plusDays(7))){
+            throw new DataInvalidaException("Data inicial não pode ser superior a 7 dias da data atual.");
+        }
+        return repository.buscarEntreDatas(dataInicial,dataFinal)
+                .stream()
+                .map(mapper::toDomain)
+                .toList();
     }
 }
