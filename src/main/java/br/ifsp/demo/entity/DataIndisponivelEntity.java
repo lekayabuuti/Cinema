67a55873a00f0241a612e96128eb631781d15dd9
brenonlps <brenolopes.xy@gmail.com>
2025-10-05 22:23:36 -0300

    feat: cria entity DataIndisponivelEntity

---
+++ src/main/java/br/ifsp/demo/entity/DataIndisponivelEntity.java
@@ -0,0 +1,17 @@
+package br.ifsp.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class DataIndisponivelEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate data;
+    private String motivo;
+
+}
