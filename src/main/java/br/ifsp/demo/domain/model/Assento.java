6e3c687263d55efe1620a8fd07bd60b7a36851f5
brenonlps <brenolopes.xy@gmail.com>
2025-10-06 19:14:02 -0300

    refactor: retira status de Assento

--- src/main/java/br/ifsp/demo/domain/model/Assento.java
+++ src/main/java/br/ifsp/demo/domain/model/Assento.java
@@ -1,16 +1,21 @@
 package br.ifsp.demo.domain.model;
 
-import br.ifsp.demo.domain.enumerations.Status;
-
 public class Assento {
     private final String codigo;
     private final Sala sala;
-    private Status status;
 
     public Assento(String codigo, Sala sala) {
         this.codigo = codigo;
         this.sala = sala;
-        this.status = Status.DISPONIVEL;
     }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
 }
 
