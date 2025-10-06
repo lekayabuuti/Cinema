c478632e92806570bfb093669f46e9a42908ab72
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-06 20:14:29 -0300

    refactor: Alterando classe Assento, colocando dois construtores: o primeiro para criar um assento com código personalizado (fileira + numero), e outro para utilizar no teste (passando o código já pronto)

--- src/main/java/br/ifsp/demo/domain/model/Assento.java
+++ src/main/java/br/ifsp/demo/domain/model/Assento.java
@@ -5,12 +5,15 @@ import br.ifsp.demo.domain.enumerations.Status;
 public class Assento {
     private final String codigo;
     private final Sala sala;
-    private Status status;
+
+    public Assento(String fileira, int numeroAssento, Sala sala) {
+        this.codigo = fileira + numeroAssento;
+        this.sala = sala;
+    }
 
     public Assento(String codigo, Sala sala) {
         this.codigo = codigo;
         this.sala = sala;
-        this.status = Status.DISPONIVEL;
     }
 }
 
