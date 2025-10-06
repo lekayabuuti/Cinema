c560b52ba5c57c99be36c81b7f9fcf6596c54c07
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-06 20:20:28 -0300

    refactor: Ajustando Reserva. Esta colocada como uma classe @Entity, agora foi ajustada para ser uma classe de Dominío, separando os arquivos de Domínio e Entity

--- src/main/java/br/ifsp/demo/domain/model/Reserva.java
+++ src/main/java/br/ifsp/demo/domain/model/Reserva.java
@@ -1,37 +1,18 @@
 package br.ifsp.demo.domain.model;
 
 import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
-@Entity
+
 public class Reserva {
 
-    @Id
-    @GeneratedValue
-    private UUID id;
+    private Long id;
 
-    @ManyToOne(optional = false)
+    // Uma reserva possuirá uma Lista de ingressos
+    private List<Ingresso> ingressos = new ArrayList<>();
+
     private Sessao sessao;
-
-    @OneToOne(optional = false)
-    private Ingresso ingresso;
-
-    public Reserva() {}
-
-    public Reserva(Sessao sessao, Ingresso ingresso) {
-        this.sessao = sessao;
-        this.ingresso = ingresso;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public Sessao getSessao() {
-        return sessao;
-    }
-
-    public Ingresso getIngresso() {
-        return ingresso;
-    }
 }
