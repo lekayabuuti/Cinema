8ba57abebe4cde76380abb01df56c3b9fda53215
brenonlps <brenolopes.xy@gmail.com>
2025-10-05 22:22:57 -0300

    refactor: adiciona validação de disponibilidade de data no método buscarSessoesEntre

--- src/main/java/br/ifsp/demo/service/SessaoService.java
+++ src/main/java/br/ifsp/demo/service/SessaoService.java
@@ -2,7 +2,7 @@ package br.ifsp.demo.service;
 
 import br.ifsp.demo.domain.model.Sessao;
 import br.ifsp.demo.exception.*;
-import br.ifsp.demo.repository.SessaoRepository;
+import br.ifsp.demo.repository.*;
 import br.ifsp.demo.mapper.SessaoMapper;
 import org.springframework.stereotype.Service;
 
@@ -11,28 +11,40 @@ import java.util.List;
 
 @Service
 public class SessaoService {
-    private final SessaoRepository repository;
     private final SessaoMapper mapper;
+    private final SessaoRepository sessaoRepository;
+    private final DataIndisponivelRepository dataIndisponivelRepository;
     private static final int LIMITE_DIAS = 7;
 
-    public SessaoService(SessaoRepository repository, SessaoMapper mapper) {
-        this.repository = repository;
+
+    public SessaoService(SessaoRepository repository, SessaoMapper mapper, DataIndisponivelRepository dataIndisponivelRepository) {
+        this.sessaoRepository = repository;
         this.mapper = mapper;
+        this.dataIndisponivelRepository = dataIndisponivelRepository;
     }
 
     public List<Sessao> buscarSessoesEntre(LocalDate dataInicial, LocalDate dataFinal) {
-        validarData(dataInicial,"Data inicial");
-        validarData(dataFinal,"Data final");
+        validarDataLimite(dataInicial,"Data inicial");
+        validarDataLimite(dataFinal,"Data final");
 
-        return repository.buscarEntreDatas(dataInicial,dataFinal)
+        validarDataDisponivel(dataInicial);
+        validarDataDisponivel(dataFinal);
+
+        return sessaoRepository.buscarEntreDatas(dataInicial,dataFinal)
                 .stream()
                 .map(mapper::toDomain)
                 .toList();
     }
 
-    private void validarData(LocalDate data, String tipo) {
+    private void validarDataLimite(LocalDate data, String tipo) {
         if (data.isAfter(LocalDate.now().plusDays(LIMITE_DIAS))) {
             throw new DataInvalidaException(tipo + " não pode ser superior a " + LIMITE_DIAS + " dias da data atual.");
+        }
+    }
+
+    private void validarDataDisponivel(LocalDate data) {
+        if(dataIndisponivelRepository.existsByData(data)){
+            throw new SessaoIndisponivelException("Data "+ data +" indisponível para sessões");
         }
     }
 }
