9b06786e293267df9ce6943733f0daece7a54525
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-06 02:44:32 -0300

    feat: adição do ReservaIngressoServiceTest

--- src/test/java/br/ifsp/demo/service/ReservaIngressoServiceTest.java
+++ src/test/java/br/ifsp/demo/service/ReservaIngressoServiceTest.java
@@ -1,19 +1,33 @@
 package br.ifsp.demo.service;
 
+import br.ifsp.demo.domain.enumerations.Status;
 import br.ifsp.demo.domain.model.*;
+import br.ifsp.demo.entity.SessaoEntity;
 import br.ifsp.demo.repository.SessaoRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+
+import org.mockito.ArgumentCaptor;
+import br.ifsp.demo.entity.IngressoEntity;
+
 
 @ExtendWith(MockitoExtension.class)
 @Tag("UnitTest")
@@ -26,7 +40,7 @@ class ReservaIngressoServiceTest {
     private ReservaIngressoService reservaIngressoService;
 
     @Test
-    @DisplayName("Deve reservar um ingresso com sucesso a sessão e o assento estão disponiveis")
+    @DisplayName("Deve reservar um ingresso com sucesso, se a sessão e o assento estiverem disponiveis")
     public void deveReservarComSucessoQuandoSessaoEAssentoEstaoDisponiveis(){
         Sala sala01 = new Sala(1);
         Filme filme01 = new Filme("Filme1", 90);
@@ -36,9 +50,29 @@ class ReservaIngressoServiceTest {
 
         Sessao sessaoTeste = new Sessao(filme01, horario01 ,sala01);
         Long sessaoTesteId = 1L;
-        sessaoTeste.setId(sessaoTesteId);
+        List<String> assentosParaReservar = List.of("A1");
 
-        when(sessaoCinemaRepository.findById(sessaoTesteId)).thenReturn(Optional.of(sessaoTeste));
+        SessaoEntity sessaoEntity = new SessaoEntity();
+        sessaoEntity.setId(sessaoTesteId);
+
+        IngressoEntity ingressoDisponivelEntity = new IngressoEntity();
+        ingressoDisponivelEntity.setAssento(new Assento("A", sala01));
+        ingressoDisponivelEntity.setStatus(Status.DISPONIVEL);
+        ingressoDisponivelEntity.setSessao(sessaoEntity);
+
+        sessaoEntity.setIngressos(new ArrayList<>(List.of(ingressoDisponivelEntity)));
+
+        when(sessaoCinemaRepository.findById(sessaoTesteId)).thenReturn(Optional.of(sessaoEntity));
+
+        reservaIngressoService.reservarIngresso(sessaoTesteId, assentosParaReservar);
+
+        ArgumentCaptor<SessaoEntity> captor = ArgumentCaptor.forClass(SessaoEntity.class);
+        verify(sessaoCinemaRepository).save(captor.capture());
+
+        SessaoEntity sessaoSalva = captor.getValue();
+        IngressoEntity ingressoSalvo = sessaoSalva.getIngressos().get(0);
+
+        assertThat(ingressoSalvo.getStatus()).isEqualTo(Status.RESERVADO);
     }
 
 
