8a9305d284d52bb99b95bb17ff7fa353b2ce48ce
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-06 21:14:39 -0300

    feat: Criando entity de Reserva (ReservaEntity)

--- src/main/java/br/ifsp/demo/entity/ReservaEntity.java
+++ src/main/java/br/ifsp/demo/entity/ReservaEntity.java
@@ -1,4 +1,24 @@
 package br.ifsp.demo.entity;
 
-public class ReservaEntity {
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "reservas")
+public class ReservaEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //gerar IDs
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sessao_id", nullable = false)
+    private SessaoEntity sessao;
+
+    @OneToMany(mappedBy = "reserva", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<IngressoEntity> ingressos = new ArrayList<>();
 }
