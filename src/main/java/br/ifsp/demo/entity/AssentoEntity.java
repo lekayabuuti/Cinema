9b6efa5d59f782e84ff19f873c113dd3c3d1a7f4
brenonlps <brenolopes.xy@gmail.com>
2025-10-06 22:59:00 -0300

    refactor: remocao de resquícios de sala

--- src/main/java/br/ifsp/demo/entity/AssentoEntity.java
+++ src/main/java/br/ifsp/demo/entity/AssentoEntity.java
@@ -14,26 +14,18 @@ public class AssentoEntity {
     @Column(nullable = false)
     private String codigo;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "sala_id", nullable = false)
-    private SalaEntity sala;
-
-
     public AssentoEntity() {}
 
-    public AssentoEntity(String codigo, SalaEntity sala) {
+    public AssentoEntity(String codigo) {
         this.codigo = codigo;
-        this.sala = sala;
     }
 
-    // Getters e Setters
+
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
     public String getCodigo() { return codigo; }
     public void setCodigo(String codigo) { this.codigo = codigo; }
 
-    public SalaEntity getSala() { return sala; }
-    public void setSala(SalaEntity sala) { this.sala = sala; }
 
 }
