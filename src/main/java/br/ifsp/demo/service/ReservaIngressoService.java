087d0dfa603bf16d182f9c82010b55eb12b0db1d
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-06 23:00:32 -0300

    feat: colocando lógica de reservar ingresso

--- src/main/java/br/ifsp/demo/service/ReservaIngressoService.java
+++ src/main/java/br/ifsp/demo/service/ReservaIngressoService.java
@@ -1,5 +1,6 @@
 package br.ifsp.demo.service;
 
+import br.ifsp.demo.domain.model.AssentoSessao;
 import br.ifsp.demo.domain.model.Ingresso;
 import br.ifsp.demo.domain.model.Reserva;
 import br.ifsp.demo.domain.model.Sessao;
@@ -25,10 +26,21 @@ public class ReservaIngressoService {
     }
 
     @Transactional
-    public Reserva reservarIngresso(Long sessaoId, List<String> assentosIds) {
+    public Ingresso reservarIngresso(Long sessaoId, String codigoAssento) { // recebe só um assento por vez
+        // 1 - carrega a SessaoEntity
         SessaoEntity sessaoEntity = sessaoRepository.findById(sessaoId)
                 .orElseThrow(() -> new EntityNotFoundException("Sessão não encontrada com o ID: " + sessaoId));
 
+        // 2- mapeia para o domínio
         Sessao sessaoDomain = SessaoMapper.toDomain(sessaoEntity);
+
+        // 3- chama a lógica de negócio no domínio
+        AssentoSessao assentoReservado = sessaoDomain.reservarAssento(codigoAssento);
+
+        // 4 - salva (o JPA detectará as mudanças na transação)
+        sessaoRepository.save(sessaoEntity);
+
+        // 5- gera o "recibo" final
+        return new Ingresso(assentoReservado);
     }
 }
