680e0b7ec5f0fc2e34726f9e3b5285ab5f2e699b
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-08 20:55:14 -0300

    feat - test(reserva): adiciona teste para reserva de ingresso com sucesso

--- src/test/java/br/ifsp/demo/service/ReservaIngressoServiceTest.java
+++ src/test/java/br/ifsp/demo/service/ReservaIngressoServiceTest.java
@@ -1,15 +1,31 @@
 package br.ifsp.demo.service;
 
 import br.ifsp.demo.aplication.service.ReservaIngressoService;
+import br.ifsp.demo.domain.enumerations.Status;
+import br.ifsp.demo.domain.model.*;
+import br.ifsp.demo.infrastructure.entity.IngressoEntity;
+import br.ifsp.demo.infrastructure.entity.SessaoEntity;
+import br.ifsp.demo.infrastructure.mapper.SessaoMapper;
 import br.ifsp.demo.infrastructure.repository.SessaoRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -17,46 +33,57 @@ import static org.mockito.ArgumentMatchers.any;
 @Tag("TDD")
 class ReservaIngressoServiceTest {
     @Mock
-    private SessaoRepository sessaoCinemaRepository;
+    private SessaoRepository sessaoRepository; // mock da camada de persistencia
+
+    @Mock
+    private SessaoMapper sessaoMapper; //mock do mapper
 
     @InjectMocks
-    private ReservaIngressoService reservaIngressoService;
-    /*
+    private ReservaIngressoService reservaIngressoService; //classe a ser testada
+
     @Test
     @DisplayName("Deve reservar um ingresso com sucesso, se a sessão e o assento estiverem disponiveis")
     public void deveReservarComSucessoQuandoSessaoEAssentoEstaoDisponiveis(){
-        Sala sala01 = new Sala(1);
-        Filme filme01 = new Filme("Filme1", 90);
-        LocalDate dataDaSessao = LocalDate.of(2025, 10, 6);
-        LocalTime horaDaSessao = LocalTime.of(19, 30);
-        Horario horario01 = new Horario(dataDaSessao, horaDaSessao);
 
-        Sessao sessaoTeste = new Sessao(filme01, horario01 ,sala01);
-        Long sessaoTesteId = 1L;
-        List<String> assentosParaReservar = List.of("A1");
+        //ARRANGE / GIVEN / dado que...
+        //1 - criando os OBJETOS DE DOMÍNIO que representam o estado inicial
+        Sala sala = new Sala(1);
+        sala.getAssentos().add(new Assento("A1"));
+        sala.getAssentos().add(new Assento("A2"));
 
-        SessaoEntity sessaoEntity = new SessaoEntity();
-        sessaoEntity.setId(sessaoTesteId);
+        Sessao sessaoDeDominio = new Sessao(new Filme("Filme legal", 90),
+                new Horario(LocalDate.now().plusDays(1), LocalTime.of(20,0)),
+                sala
+        );
+        Long sessaoId = 1L;
+        String assentoParaReservar = "A1";
 
-        IngressoEntity ingressoDisponivelEntity = new IngressoEntity();
-        ingressoDisponivelEntity.setAssento(new Assento("A", sala01));
-        ingressoDisponivelEntity.setStatus(Status.DISPONIVEL);
-        ingressoDisponivelEntity.setSessao(sessaoEntity);
+        //2- Criando uma entidade falsa que o repositorio "retornará do banco de dados"
+        //nao é necessario ter todos os dados, pois só o ID é suficiente para o mock.
+        SessaoEntity sessaoEntityFalsa = new SessaoEntity();
+        sessaoEntityFalsa.setId(sessaoId);
 
-        sessaoEntity.setIngressos(new ArrayList<>(List.of(ingressoDisponivelEntity)));
+        //3 - Configurando os mocks.
+        // quando o repositorio procurar pelo Id, retorna a entidade falsa
+        when(sessaoRepository.findById(sessaoId)).thenReturn(Optional.of(sessaoEntityFalsa));
+        // qnd o mapper for converter a entidade falsa, retornará objeto de dominio compleeto
+        when(sessaoMapper.toDomain(any(SessaoEntity.class))).thenReturn(sessaoDeDominio);
 
-        when(sessaoCinemaRepository.findById(sessaoTesteId)).thenReturn(Optional.of(sessaoEntity));
 
-        reservaIngressoService.reservarIngresso(sessaoTesteId, assentosParaReservar);
+        // ACT / WHEN / Quando...
 
-        ArgumentCaptor<SessaoEntity> captor = ArgumentCaptor.forClass(SessaoEntity.class);
-        verify(sessaoCinemaRepository).save(captor.capture());
+        // O serviço retorna o "recibo" da operação
+        Ingresso ingressoGerado = reservaIngressoService.reservarIngresso(sessaoId, assentoParaReservar);
 
-        SessaoEntity sessaoSalva = captor.getValue();
-        IngressoEntity ingressoSalvo = sessaoSalva.getIngressos().get(0);
+        // ASSERT / THEN / Então...
+        //1 - verificar se o recibo foi gerado corretamente
+        assertThat(ingressoGerado).isNotNull();
+        assertThat(ingressoGerado.getCodigoAssento()).isEqualTo(assentoParaReservar);
+        assertThat(ingressoGerado.getFilme().nome()).isEqualTo("Filme legal");
 
-        assertThat(ingressoSalvo.getStatus()).isEqualTo(Status.RESERVADO);
+        //verificar se a sessao foi salva
+        verify(sessaoRepository).save(any(SessaoEntity.class));
     }
-    */
+
 
 }
