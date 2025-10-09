939ef26c721beec1cf8b39447be35f5c9a6238bf
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-09 00:49:17 -0300

    feat: Criando nova exception para assento indisponivel

---
+++ src/main/java/br/ifsp/demo/domain/exception/AssentoIndisponivelException.java
@@ -0,0 +1,7 @@
+package br.ifsp.demo.domain.exception;
+
+public class AssentoIndisponivelException extends RuntimeException{
+    public AssentoIndisponivelException(String message) {
+        super(message);
+    }
+}
