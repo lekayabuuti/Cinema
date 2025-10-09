bed276605fa6cddb8e46680232d5338edeb72637
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-08 21:22:28 -0300

    feat: Adicionando método para reservar assento em Sessao

--- src/main/java/br/ifsp/demo/domain/model/Sessao.java
+++ src/main/java/br/ifsp/demo/domain/model/Sessao.java
@@ -25,4 +25,29 @@ public class Sessao {
         // pra cada assento físico na sala, cria um controle de status para esta sessão.
         this.assentosDaSessao = new ArrayList<>();
     }
+
+    /**
+     * Encontra um assento pelo código, aplica a regra de negócio de reserva
+     * e altera o estado do assento.
+     * @param codigoAssento O código do assento a ser reservado (ex: "A1").
+     * @return O objeto AssentoSessao que foi reservado.
+     */
+    public AssentoSessao reservarAssento (String codigoAssento){
+        //1 - Encontra o assento desejado na lista de assentos DESSA sessao
+        AssentoSessao assentoParaReservar = this.assentosDaSessao.stream()
+                .filter(assentoSessao -> assentoSessao.getAssento().getCodigo().equals(codigoAssento))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Assento não encontrado nessa sessão"));
+
+        // 2- Aplica a regra de negocio (verificar se esta disponivel SOMENTE... por enquanto)
+        if (!assentoParaReservar.estaDisponivel()){
+            throw new RuntimeException("Assento já está reservado");
+        }
+
+        //3 manda o objeto se reservar (encapsulamento)
+        assentoParaReservar.reservar();
+
+        return assentoParaReservar;
+
+    }
 }
