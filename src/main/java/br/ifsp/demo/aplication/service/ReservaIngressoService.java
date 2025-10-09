0312cd0ad70ee0bf4e131f35e060c7185cbe84b4
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-08 22:47:51 -0300

    fix: excluindo código comentado de Reserva Ingresso Service

--- src/main/java/br/ifsp/demo/aplication/service/ReservaIngressoService.java
+++ src/main/java/br/ifsp/demo/aplication/service/ReservaIngressoService.java
@@ -36,27 +36,5 @@ public class ReservaIngressoService {
         sessaoRepository.save(entityParaSalvar);
 
         return new Ingresso(assentoReservado);
-
-
-
     }
-//    @Transactional
-//    public Ingresso reservarIngresso(Long sessaoId, String codigoAssento) { // recebe só um assento por vez
-//        // 1 - carrega a SessaoEntity
-//        SessaoEntity sessaoEntity = sessaoRepository.findById(sessaoId)
-//                .orElseThrow(() -> new EntityNotFoundException("Sessão não encontrada com o ID: " + sessaoId));
-//
-//        // 2- mapeia para o domínio
-//        Sessao sessaoDomain = SessaoMapper.toDomain(sessaoEntity);
-//
-//        // 3- chama a lógica de negócio no domínio
-//        AssentoSessao assentoReservado = sessaoDomain.reservarAssento(codigoAssento);
-//
-//        // 4 - salva (o JPA detectará as mudanças na transação)
-//        sessaoRepository.save(sessaoEntity);
-//
-//        // 5- gera o "recibo" final
-//        return new Ingresso(assentoReservado);
-//    }
-
 }
