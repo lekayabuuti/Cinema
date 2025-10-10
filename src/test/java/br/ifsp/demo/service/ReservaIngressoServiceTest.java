050e9aad69dd2d673c3311738c21e9d84d749749
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-10 13:14:34 -0300

    feat(teste): Cria um teste para verificar se o id da Sessao é inexistente, lançando uma SessaoLotadaException
    -closes #10

--- src/test/java/br/ifsp/demo/service/ReservaIngressoServiceTest.java
+++ src/test/java/br/ifsp/demo/service/ReservaIngressoServiceTest.java
@@ -1,6 +1,7 @@
 package br.ifsp.demo.service;
 
 import br.ifsp.demo.domain.exception.SessaoIndisponivelException;
+import br.ifsp.demo.domain.exception.SessaoInexistenteException;
 import br.ifsp.demo.domain.exception.SessaoLotadaException;
 import br.ifsp.demo.domain.service.ReservaIngressoService;
 import br.ifsp.demo.domain.exception.AssentoIndisponivelException;
@@ -55,7 +56,6 @@ class ReservaIngressoServiceTest {
         sessaoEntityFalsa.setId(sessaoId);
 
         //configuração dos mocks
-        when(sessaoRepository.findById(sessaoId)).thenReturn(Optional.of(sessaoEntityFalsa));
     }
 
     @Test
@@ -63,6 +63,7 @@ class ReservaIngressoServiceTest {
     public void deveReservarComSucessoQuandoSessaoEAssentoEstaoDisponiveis(){
         //ARRANGE
         String assentoParaReservar = "A1";
+        when(sessaoRepository.findById(sessaoId)).thenReturn(Optional.of(sessaoEntityFalsa));
         when(sessaoMapper.toDomain(any(SessaoEntity.class))).thenReturn(sessaoDomain);
         when(sessaoMapper.toEntity(any(Sessao.class))).thenReturn(new SessaoEntity());
 
@@ -83,6 +84,7 @@ class ReservaIngressoServiceTest {
     public void deveLancarAssentoIndisponivelExceptionQuandoAssentoJaEstiverReservado(){
         String assentoJaReservado = "A1";
         sessaoDomain.reservarAssento(assentoJaReservado);
+        when(sessaoRepository.findById(sessaoId)).thenReturn(Optional.of(sessaoEntityFalsa));
         when(sessaoMapper.toDomain(any(SessaoEntity.class))).thenReturn(sessaoDomain);
 
         Assertions.assertThrows(AssentoIndisponivelException.class, () ->{
@@ -107,7 +109,7 @@ class ReservaIngressoServiceTest {
                 dataHoraPassada,
                 sala
         );
-
+        when(sessaoRepository.findById(sessaoId)).thenReturn(Optional.of(sessaoEntityFalsa));
         when(sessaoMapper.toDomain(any(SessaoEntity.class))).thenReturn(sessaoEncerrada);
 
         String assentoParaReservar = "A1";
@@ -126,12 +128,30 @@ class ReservaIngressoServiceTest {
         sessaoDomain.reservarAssento("A1");
         sessaoDomain.reservarAssento("A2");
 
+        when(sessaoRepository.findById(sessaoId)).thenReturn(Optional.of(sessaoEntityFalsa));
         when(sessaoMapper.toDomain(any(SessaoEntity.class))).thenReturn(sessaoDomain);
 
         Assertions.assertThrows(SessaoLotadaException.class, () -> {
             reservaIngressoService.reservarIngresso(sessaoId, "A1");
         });
 
+        verify(sessaoRepository, never()).save(any());
+    }
+
+    @Test
+    @DisplayName("Deve lançar SessaoInexistenteException quando tentar reservar um ou mais ingressos em uma sessão inexistente")
+    void deveLancarSessaoInexistenteExceptionQuandoTentarReservarIngressoEmUmaSessaoInexistente(){
+        Long idInexistente = 999L;
+        String qualquerAssento = "A1";
+
+        //simula um "não encontrado" no banco de dados
+        when(sessaoRepository.findById(idInexistente)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(SessaoInexistenteException.class, ()->{
+            reservaIngressoService.reservarIngresso(idInexistente, qualquerAssento);
+        });
+
+        verify(sessaoMapper, never()).toDomain(any());
         verify(sessaoRepository, never()).save(any());
     }
 }
