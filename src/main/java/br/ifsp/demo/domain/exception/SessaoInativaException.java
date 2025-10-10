db678d98e501ecb02772495bcd21ec67fd55d42a
brenonlps <brenolopes.xy@gmail.com>
2025-10-10 10:35:43 -0300

    feat(exception): Cria SessaoInativaException e handler para API REST

---
+++ src/main/java/br/ifsp/demo/domain/exception/SessaoInativaException.java
@@ -0,0 +1,7 @@
+package br.ifsp.demo.domain.exception;
+
+public class SessaoInativaException extends RuntimeException {
+    public SessaoInativaException(String message) {
+        super(message);
+    }
+}
