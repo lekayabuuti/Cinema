ae83c8d052749323c98357991e60a565b7ba2c13
brenonlps <brenolopes.xy@gmail.com>
2025-10-10 23:27:55 -0300

    test(assento): Adiciona teste unitário de defesa contra sessaoId nulo na Application (#52)

---
+++ src/test/java/br/ifsp/demo/application/AssentoSessaoServiceTest.java
@@ -0,0 +1,46 @@
+package br.ifsp.demo.application;
+
+import br.ifsp.demo.application.service.AssentoSessaoService;
+import br.ifsp.demo.domain.exception.SessaoInexistenteException;
+import br.ifsp.demo.domain.model.Assento;
+import br.ifsp.demo.domain.model.Sessao;
+import br.ifsp.demo.domain.repository.AssentoSessaoRepository;
+import br.ifsp.demo.domain.service.SessaoService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@Tag("UnitTest")
+@ExtendWith(MockitoExtension.class)
+public class AssentoSessaoServiceTest {
+    @Mock
+    AssentoSessaoRepository assentoSessaoRepository;
+    @Mock
+    SessaoService sessaoService;
+
+    @Mock
+    Assento assentoMock;
+    @Mock
+    Sessao sessaoMock;
+
+    @InjectMocks
+    AssentoSessaoService assentoSessaoService;
+
+    // #52
+    @Test
+    @Tag("UnitTest")
+    @DisplayName("Deve lançar IllegalArgumentException quando o ID da sessão for nulo (defesa #52)")
+    void deveLancarExceptionQuandoIdSessaoForNulo() {
+        assertThrows(IllegalArgumentException.class, () -> assentoSessaoService.buscarPorSessao(null, false));
+    }
+
+
+
+}
