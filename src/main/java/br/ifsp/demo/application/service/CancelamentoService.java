ac2c3388f353888a94edb4f0c3a3bdc77feb0e5c
LucaoFaz <lucasmatheussantos12@hotmail.com>
2025-10-10 21:18:21 -0300

    feat: Cria CancelamentoService com função principal de cancelar (cancela uma reserva).
    closes #12

--- src/main/java/br/ifsp/demo/application/service/CancelamentoService.java
+++ src/main/java/br/ifsp/demo/application/service/CancelamentoService.java
@@ -1,4 +1,36 @@
 package br.ifsp.demo.application.service;
 
+import br.ifsp.demo.domain.exception.SessaoInexistenteException;
+import br.ifsp.demo.domain.model.Sessao;
+import br.ifsp.demo.infrastructure.persistence.entity.SessaoEntity;
+import br.ifsp.demo.infrastructure.persistence.mapper.SessaoMapper;
+import br.ifsp.demo.infrastructure.persistence.repository.SessaoRepository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public class CancelamentoService {
+    private final SessaoRepository sessaoRepository;
+    private final SessaoMapper sessaoMapper;
+
+    public CancelamentoService(SessaoRepository sessaoRepository, SessaoMapper sessaoMapper) {
+        this.sessaoRepository = sessaoRepository;
+        this.sessaoMapper = sessaoMapper;
+    }
+
+    @Transactional
+    public void cancelar(Long sessaoId, String assentoParaCancelar){
+        //1 criar sessao entity (puxar a tabela do banco)
+        SessaoEntity sessaoEntity = sessaoRepository.findById(sessaoId)
+                .orElseThrow(() -> new SessaoInexistenteException("Sessão não encontrada"));
+
+        Sessao sessaoDomain = sessaoMapper.toDomain(sessaoEntity);
+
+        sessaoDomain.cancelarReserva(assentoParaCancelar, LocalDateTime.now());
+
+        SessaoEntity entityParaSalvar = sessaoMapper.toEntity(sessaoDomain);
+        sessaoRepository.save(entityParaSalvar);
+    }
 }
