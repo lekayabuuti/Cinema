9d14c952f6fc952dca357d5c6775c4a21ad3b776
brenonlps <brenolopes.xy@gmail.com>
2025-11-12 11:30:11 -0300

    feat: adicionar relacionamento entre ReservaEntity e User

--- src/main/java/br/ifsp/demo/infrastructure/persistence/entity/ReservaEntity.java
+++ src/main/java/br/ifsp/demo/infrastructure/persistence/entity/ReservaEntity.java
@@ -1,5 +1,6 @@
 package br.ifsp.demo.infrastructure.persistence.entity;
 
+import br.ifsp.demo.infrastructure.security.user.User;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -18,6 +19,10 @@ public class ReservaEntity implements Serializable {
     @ManyToOne
     @JoinColumn(name = "sessao_id", nullable = false)
     private SessaoEntity sessao;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private User usuario;
 
     @OneToMany(mappedBy = "reserva", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<IngressoEntity> ingressos = new ArrayList<>();
